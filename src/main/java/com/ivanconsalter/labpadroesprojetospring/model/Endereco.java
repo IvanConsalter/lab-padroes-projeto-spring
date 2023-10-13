31dd8528613fdf8f8aa7c00a8e3da474093a684a
Ivan Consalter <ivanconsalter1@gmail.com>
2023-10-13 14:32:44 -0300

    added structure for Endereco

---
+++ src/main/java/com/ivanconsalter/labpadroesprojetospring/model/Endereco.java
@@ -0,0 +1,5 @@
+package com.ivanconsalter.labpadroesprojetospring.model;
+
+public class Endereco {
+
+}
---
+++ src/main/java/com/ivanconsalter/labpadroesprojetospring/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.ivanconsalter.labpadroesprojetospring.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ivanconsalter.labpadroesprojetospring.model.Endereco;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+ 
+}
