fa86dbdd26c20cc77f594f9e8a5b6624606df409
Ivan Consalter <ivanconsalter1@gmail.com>
2023-10-13 15:28:12 -0300

    added crud

--- src/main/java/com/ivanconsalter/labpadroesprojetospring/model/Cliente.java
+++ src/main/java/com/ivanconsalter/labpadroesprojetospring/model/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Cliente {
@@ -13,6 +14,9 @@ public class Cliente {
 	private Long id;
 	
 	private String nome;
+	
+	@ManyToOne
+	private Endereco endereco;
 
 	public Long getId() {
 		return id;
@@ -28,6 +32,14 @@ public class Cliente {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
 	}
 
 }
--- src/main/java/com/ivanconsalter/labpadroesprojetospring/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/ivanconsalter/labpadroesprojetospring/service/impl/ClienteServiceImpl.java
@@ -1,41 +1,80 @@
 package com.ivanconsalter.labpadroesprojetospring.service.impl;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ivanconsalter.labpadroesprojetospring.model.Cliente;
+import com.ivanconsalter.labpadroesprojetospring.model.Endereco;
+import com.ivanconsalter.labpadroesprojetospring.repository.ClienteRepository;
+import com.ivanconsalter.labpadroesprojetospring.repository.EnderecoRepository;
 import com.ivanconsalter.labpadroesprojetospring.service.ClienteService;
+import com.ivanconsalter.labpadroesprojetospring.service.ViaCepService;
+
+/**
+ * Implementação da <b>Strategy</b> {@link ClienteService}, a qual pode ser
+ * injetada pelo Spring (via {@link Autowired}). Com isso, como essa classe é um
+ * {@link Service}, ela será tratada como um <b>Singleton</b>.
+ * 
+ * @author ivanconsalter
+ */
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
+	// Singleton: Injetar os componentes do Spring com @Autowired.
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private ViaCepService viaCepService;
+	
+	// Strategy: Implementar os métodos definidos na interface.
+	// Facade: Abstrair integrações com subsistemas, provendo uma interface simples.
+
 	@Override
 	public Iterable<Cliente> buscarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		return clienteRepository.findAll();
 	}
 
 	@Override
 	public Cliente buscarPorId(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.get();
 	}
 
 	@Override
 	public void inserir(Cliente cliente) {
-		// TODO Auto-generated method stub
-		
+		salvarClienteComCep(cliente);
 	}
 
 	@Override
 	public void atualizar(Long id, Cliente cliente) {
-		// TODO Auto-generated method stub
-		
+		Optional<Cliente> clienteBd = clienteRepository.findById(id);
+		if (clienteBd.isPresent()) {
+			salvarClienteComCep(cliente);
+		}
 	}
 
 	@Override
 	public void deletar(Long id) {
-		// TODO Auto-generated method stub
-		
+		clienteRepository.deleteById(id);
 	}
+
+	private void salvarClienteComCep(Cliente cliente) {
+		String cep = cliente.getEndereco().getCep();
+		Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+			Endereco novoEndereco = viaCepService.consultarCep(cep);
+			enderecoRepository.save(novoEndereco);
+			return novoEndereco;
+		});
+		cliente.setEndereco(endereco);
+		clienteRepository.save(cliente);
+	}
+
 
 }
